6fa4f8451e6cfe69665dbd7e84d64dbae4aba60e
the-monitor-lizard <60822848+the-monitor-lizard@users.noreply.github.com>
2025-04-20 21:36:02 -0700

    Add files via upload.
    Code files for the JATOS/JsPsych project just to make sure that they are saved on cloud storage in case of any issue. Latest upload is working.

---
+++ stimuli.js
@@ -0,0 +1,587 @@
+var test_stimuli = [
+    {
+      "stimulus": "media/en_1.mp3"
+    },
+    {
+      "stimulus": "media/en_2.mp3"
+    },
+    {
+      "stimulus": "media/en_3.mp3"
+    },
+    {
+      "stimulus": "media/en_4.mp3"
+    },
+    {
+      "stimulus": "media/en_5.mp3"
+    },
+    {
+      "stimulus": "media/en_6.mp3"
+    },
+    {
+      "stimulus": "media/en_7.mp3"
+    },
+    {
+      "stimulus": "media/en_8.mp3"
+    },
+    {
+      "stimulus": "media/en_9.mp3"
+    },
+    {
+      "stimulus": "media/en_10.mp3"
+    },
+    {
+      "stimulus": "media/en_11.mp3"
+    },
+    {
+      "stimulus": "media/en_12.mp3"
+    },
+    {
+      "stimulus": "media/en_13.mp3"
+    },
+    {
+      "stimulus": "media/en_14.mp3"
+    },
+    {
+      "stimulus": "media/en_15.mp3"
+    },
+    {
+      "stimulus": "media/en_16.mp3"
+    },
+    {
+      "stimulus": "media/en_17.mp3"
+    },
+    {
+      "stimulus": "media/en_18.mp3"
+    },
+    {
+      "stimulus": "media/en_19.mp3"
+    },
+    {
+      "stimulus": "media/en_20.mp3"
+    },
+    {
+      "stimulus": "media/en_21.mp3"
+    },
+    {
+      "stimulus": "media/en_22.mp3"
+    },
+    {
+      "stimulus": "media/en_23.mp3"
+    },
+    {
+      "stimulus": "media/en_24.mp3"
+    },
+    {
+      "stimulus": "media/en_25.mp3"
+    },
+    {
+      "stimulus": "media/en_26.mp3"
+    },
+    {
+      "stimulus": "media/en_27.mp3"
+    },
+    {
+      "stimulus": "media/en_28.mp3"
+    },
+    {
+      "stimulus": "media/en_29.mp3"
+    },
+    {
+      "stimulus": "media/en_30.mp3"
+    },
+    {
+      "stimulus": "media/en_31.mp3"
+    },
+    {
+      "stimulus": "media/en_32.mp3"
+    },
+    {
+      "stimulus": "media/en_33.mp3"
+    },
+    {
+      "stimulus": "media/en_34.mp3"
+    },
+    {
+      "stimulus": "media/en_35.mp3"
+    },
+    {
+      "stimulus": "media/en_36.mp3"
+    },
+    {
+      "stimulus": "media/en_37.mp3"
+    },
+    {
+      "stimulus": "media/en_38.mp3"
+    },
+    {
+      "stimulus": "media/en_39.mp3"
+    },
+    {
+      "stimulus": "media/en_40.mp3"
+    },
+    {
+      "stimulus": "media/en_41.mp3"
+    },
+    {
+      "stimulus": "media/en_42.mp3"
+    },
+    {
+      "stimulus": "media/en_43.mp3"
+    },
+    {
+      "stimulus": "media/en_44.mp3"
+    },
+    {
+      "stimulus": "media/en_45.mp3"
+    },
+    {
+      "stimulus": "media/en_46.mp3"
+    },
+    {
+      "stimulus": "media/en_47.mp3"
+    },
+    {
+      "stimulus": "media/en_48.mp3"
+    },
+    {
+      "stimulus": "media/en_49.mp3"
+    },
+    {
+      "stimulus": "media/en_50.mp3"
+    },
+    {
+      "stimulus": "media/en_51.mp3"
+    },
+    {
+      "stimulus": "media/en_52.mp3"
+    },
+    {
+      "stimulus": "media/en_53.mp3"
+    },
+    {
+      "stimulus": "media/en_54.mp3"
+    },
+    {
+      "stimulus": "media/en_55.mp3"
+    },
+    {
+      "stimulus": "media/en_56.mp3"
+    },
+    {
+      "stimulus": "media/en_57.mp3"
+    },
+    {
+      "stimulus": "media/en_58.mp3"
+    },
+    {
+      "stimulus": "media/en_59.mp3"
+    },
+    {
+      "stimulus": "media/en_60.mp3"
+    },
+    {
+      "stimulus": "media/en_61.mp3"
+    },
+    {
+      "stimulus": "media/en_62.mp3"
+    },
+    {
+      "stimulus": "media/en_63.mp3"
+    },
+    {
+      "stimulus": "media/en_64.mp3"
+    },
+    {
+      "stimulus": "media/en_65.mp3"
+    },
+    {
+      "stimulus": "media/en_66.mp3"
+    },
+    {
+      "stimulus": "media/en_67.mp3"
+    },
+    {
+      "stimulus": "media/en_68.mp3"
+    },
+    {
+      "stimulus": "media/en_69.mp3"
+    },
+    {
+      "stimulus": "media/en_70.mp3"
+    },
+    {
+      "stimulus": "media/en_71.mp3"
+    },
+    {
+      "stimulus": "media/en_72.mp3"
+    },
+    {
+      "stimulus": "media/en_73.mp3"
+    },
+    {
+      "stimulus": "media/en_74.mp3"
+    },
+    {
+      "stimulus": "media/en_75.mp3"
+    },
+    {
+      "stimulus": "media/en_76.mp3"
+    },
+    {
+      "stimulus": "media/en_77.mp3"
+    },
+    {
+      "stimulus": "media/en_78.mp3"
+    },
+    {
+      "stimulus": "media/en_79.mp3"
+    },
+    {
+      "stimulus": "media/en_80.mp3"
+    },
+    {
+      "stimulus": "media/en_81.mp3"
+    },
+    {
+      "stimulus": "media/en_82.mp3"
+    },
+    {
+      "stimulus": "media/en_83.mp3"
+    },
+    {
+      "stimulus": "media/en_84.mp3"
+    },
+    {
+      "stimulus": "media/en_85.mp3"
+    },
+    {
+      "stimulus": "media/en_86.mp3"
+    },
+    {
+      "stimulus": "media/en_87.mp3"
+    },
+    {
+      "stimulus": "media/en_88.mp3"
+    },
+    {
+      "stimulus": "media/en_89.mp3"
+    },
+    {
+      "stimulus": "media/en_90.mp3"
+    },
+    {
+      "stimulus": "media/en_91.mp3"
+    },
+    {
+      "stimulus": "media/en_92.mp3"
+    },
+    {
+      "stimulus": "media/en_93.mp3"
+    },
+    {
+      "stimulus": "media/en_94.mp3"
+    },
+    {
+      "stimulus": "media/en_95.mp3"
+    },
+    {
+      "stimulus": "media/en_96.mp3"
+    },
+    {
+      "stimulus": "media/en_97.mp3"
+    },
+    {
+      "stimulus": "media/en_98.mp3"
+    },
+    {
+      "stimulus": "media/en_99.mp3"
+    },
+    {
+      "stimulus": "media/en_100.mp3"
+    },
+    {
+      "stimulus": "media/en_101.mp3"
+    },
+    {
+      "stimulus": "media/en_102.mp3"
+    },
+    {
+      "stimulus": "media/en_103.mp3"
+    },
+    {
+      "stimulus": "media/en_104.mp3"
+    },
+    {
+      "stimulus": "media/en_105.mp3"
+    },
+    {
+      "stimulus": "media/en_106.mp3"
+    },
+    {
+      "stimulus": "media/en_107.mp3"
+    },
+    {
+      "stimulus": "media/en_108.mp3"
+    },
+    {
+      "stimulus": "media/en_109.mp3"
+    },
+    {
+      "stimulus": "media/en_110.mp3"
+    },
+    {
+      "stimulus": "media/en_111.mp3"
+    },
+    {
+      "stimulus": "media/en_112.mp3"
+    },
+    {
+      "stimulus": "media/en_113.mp3"
+    },
+    {
+      "stimulus": "media/en_114.mp3"
+    },
+    {
+      "stimulus": "media/en_115.mp3"
+    },
+    {
+      "stimulus": "media/en_116.mp3"
+    },
+    {
+      "stimulus": "media/en_117.mp3"
+    },
+    {
+      "stimulus": "media/en_118.mp3"
+    },
+    {
+      "stimulus": "media/en_119.mp3"
+    },
+    {
+      "stimulus": "media/en_120.mp3"
+    },
+    {
+      "stimulus": "media/en_121.mp3"
+    },
+    {
+      "stimulus": "media/en_122.mp3"
+    },
+    {
+      "stimulus": "media/en_123.mp3"
+    },
+    {
+      "stimulus": "media/en_124.mp3"
+    },
+    {
+      "stimulus": "media/en_125.mp3"
+    },
+    {
+      "stimulus": "media/en_126.mp3"
+    },
+    {
+      "stimulus": "media/en_127.mp3"
+    },
+    {
+      "stimulus": "media/en_128.mp3"
+    },
+    {
+      "stimulus": "media/en_129.mp3"
+    },
+    {
+      "stimulus": "media/en_130.mp3"
+    },
+    {
+      "stimulus": "media/en_131.mp3"
+    },
+    {
+      "stimulus": "media/en_132.mp3"
+    },
+    {
+      "stimulus": "media/en_133.mp3"
+    },
+    {
+      "stimulus": "media/en_134.mp3"
+    },
+    {
+      "stimulus": "media/en_135.mp3"
+    },
+    {
+      "stimulus": "media/en_136.mp3"
+    },
+    {
+      "stimulus": "media/en_137.mp3"
+    },
+    {
+      "stimulus": "media/en_138.mp3"
+    },
+    {
+      "stimulus": "media/en_139.mp3"
+    },
+    {
+      "stimulus": "media/en_140.mp3"
+    },
+    {
+      "stimulus": "media/en_141.mp3"
+    },
+    {
+      "stimulus": "media/en_142.mp3"
+    },
+    {
+      "stimulus": "media/en_143.mp3"
+    },
+    {
+      "stimulus": "media/en_144.mp3"
+    },
+    {
+      "stimulus": "media/en_145.mp3"
+    },
+    {
+      "stimulus": "media/en_146.mp3"
+    },
+    {
+      "stimulus": "media/en_147.mp3"
+    },
+    {
+      "stimulus": "media/en_148.mp3"
+    },
+    {
+      "stimulus": "media/en_149.mp3"
+    },
+    {
+      "stimulus": "media/en_150.mp3"
+    },
+    {
+      "stimulus": "media/en_151.mp3"
+    },
+    {
+      "stimulus": "media/en_152.mp3"
+    },
+    {
+      "stimulus": "media/en_153.mp3"
+    },
+    {
+      "stimulus": "media/en_154.mp3"
+    },
+    {
+      "stimulus": "media/en_155.mp3"
+    },
+    {
+      "stimulus": "media/en_156.mp3"
+    },
+    {
+      "stimulus": "media/en_157.mp3"
+    },
+    {
+      "stimulus": "media/en_158.mp3"
+    },
+    {
+      "stimulus": "media/en_159.mp3"
+    },
+    {
+      "stimulus": "media/en_160.mp3"
+    },
+    {
+      "stimulus": "media/en_161.mp3"
+    },
+    {
+      "stimulus": "media/en_162.mp3"
+    },
+    {
+      "stimulus": "media/en_163.mp3"
+    },
+    {
+      "stimulus": "media/en_164.mp3"
+    },
+    {
+      "stimulus": "media/en_165.mp3"
+    },
+    {
+      "stimulus": "media/en_166.mp3"
+    },
+    {
+      "stimulus": "media/en_167.mp3"
+    },
+    {
+      "stimulus": "media/en_168.mp3"
+    },
+    {
+      "stimulus": "media/en_169.mp3"
+    },
+    {
+      "stimulus": "media/en_170.mp3"
+    },
+    {
+      "stimulus": "media/en_171.mp3"
+    },
+    {
+      "stimulus": "media/en_172.mp3"
+    },
+    {
+      "stimulus": "media/en_173.mp3"
+    },
+    {
+      "stimulus": "media/en_174.mp3"
+    },
+    {
+      "stimulus": "media/en_175.mp3"
+    },
+    {
+      "stimulus": "media/en_176.mp3"
+    },
+    {
+      "stimulus": "media/en_177.mp3"
+    },
+    {
+      "stimulus": "media/en_178.mp3"
+    },
+    {
+      "stimulus": "media/en_179.mp3"
+    },
+    {
+      "stimulus": "media/en_180.mp3"
+    },
+    {
+      "stimulus": "media/en_181.mp3"
+    },
+    {
+      "stimulus": "media/en_182.mp3"
+    },
+    {
+      "stimulus": "media/en_183.mp3"
+    },
+    {
+      "stimulus": "media/en_184.mp3"
+    },
+    {
+      "stimulus": "media/en_185.mp3"
+    },
+    {
+      "stimulus": "media/en_186.mp3"
+    },
+    {
+      "stimulus": "media/en_187.mp3"
+    },
+    {
+      "stimulus": "media/en_188.mp3"
+    },
+    {
+      "stimulus": "media/en_189.mp3"
+    },
+    {
+      "stimulus": "media/en_190.mp3"
+    },
+    {
+      "stimulus": "media/en_191.mp3"
+    },
+    {
+      "stimulus": "media/en_192.mp3"
+    },
+    {
+      "stimulus": "media/en_193.mp3"
+    },
+    {
+      "stimulus": "media/en_194.mp3"
+    },
+    {
+      "stimulus": "media/en_195.mp3"
+    }
+  ]
